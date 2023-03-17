743bfbd5f26c4b66c56e86ee1210a9a4b22e8751
chris051819 <christian.lescano07@gmail.com>
2023-03-17 20:31:42 +0800

    Create Container
    Added uploading/deleting/saving image section in createContainer section. Loader.jsx also added for additional style while uploading image.

--- Capstone/my-app/src/components/Header.jsx
+++ Capstone/my-app/src/components/Header.jsx
@@ -111,7 +111,7 @@ const Header = () => {
                   <Link to={"/createItem"}>
                     <p
                       className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100
-                transition-all duration-100 ease-in-out text-textColor text-base"
+                transition-all duration-100 ease-in-out text-textColor text-base"  onClick={() => setusMenu(false)}
                     >
                       New Item <MdAdd />
                     </p>
@@ -164,6 +164,7 @@ const Header = () => {
                   <p
                     className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100
                 transition-all duration-100 ease-in-out text-textColor text-base"
+                onClick={() => setusMenu(false)}
                   >
                     New Item <MdAdd />
                   </p>
@@ -173,25 +174,26 @@ const Header = () => {
               <ul className="flex flex-col gap-6 px-4 py-2">
                 <li
                   className="text-base text-textColor hover:text-headingColor duration-100
-                    transition-all ease-in-out cursor-pointer hover:bg-slate-100"
+                    transition-all ease-in-out cursor-pointer hover:bg-slate-100"  onClick={() => setusMenu(false)}
                 >
                   Home
                 </li>
                 <li
                   className="text-base text-textColor hover:text-headingColor duration-100
-                    transition-all ease-in-out cursor-pointer hover:bg-slate-100"
+                    transition-all ease-in-out cursor-pointer hover:bg-slate-100"  onClick={() => setusMenu(false)}
                 >
                   Menu
                 </li>
                 <li
                   className="text-base text-textColor hover:text-headingColor duration-100
-            transition-all ease-in-out cursor-pointer hover:bg-slate-100"
+            transition-all ease-in-out cursor-pointer hover:bg-slate-100"  onClick={() => setusMenu(false)}
                 >
                   About Us
                 </li>
                 <li
                   className="text-base text-textColor hover:text-headingColor duration-100
-            transition-all ease-in-out cursor-pointer hover:bg-slate-100"
+            transition-all ease-in-out cursor-pointer hover:bg-slate-100"  
+            onClick={() => setusMenu(false)}
                 >
                   Service
                 </li>
