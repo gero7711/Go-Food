84316d2ad2859347c246c656626132415da3c20c
gero7711 <106569362+gero7711@users.noreply.github.com>
2023-03-19 20:16:06 +0800

    my edit in main container and row container

--- Capstone/my-app/src/components/RowContainer.jsx
+++ Capstone/my-app/src/components/RowContainer.jsx
@@ -1,7 +1,8 @@
 import React from "react"
 
-const RowContainer = () => {
-    return <div className={'w-full my-12 bg-black'}></div>;
+const RowContainer = ({flag}) => {
+    return <div className={'w-full my-12 bg-black ${flag ? 'overflow-x-hidden' : '}></div>;
 };
 
 export default RowContainer;
+
